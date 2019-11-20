6e3b04e253ec747708de6263da16f397098d7fbe
mihailnetkachev <mihailnetkachev.8@gmail.com>
2019-11-20 20:45:25 +0200

    Naming-fix for App.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Routing from './routing';
 import { combineStrings as cs } from '../../helpers';
 
-import { app as appClass } from './app.module.sass';
+import { container as containerClass } from './app.module.sass';
 
 class App extends React.Component {
 
   render () {
     return (
-      <div className={cs()(appClass, 'appContainer')}>
+      <div className={cs()(containerClass, 'appContainer')}>
         <header>{}</header>
         <main>
           <Routing/>

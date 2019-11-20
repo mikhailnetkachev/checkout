741de31d00e843d48877ef918c4a67de463ddcfd
mihailnetkachev <mihailnetkachev.8@gmail.com>
2019-11-20 19:07:59 +0200

    Structure building.

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {} from './app.module.sass';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div>Application is here!</div>
+    );
+  }
+}
+
+export default App;
